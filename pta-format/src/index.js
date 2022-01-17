dba556f0d4ddbd81135a882e41a3d100f97968d1
Carlo <carlo.panzi@gmail.com>
2022-01-17 11:48:26 +0100

    feat: Support for multiple files in input

--- pta-format/src/index.js
+++ pta-format/src/index.js
@@ -2,6 +2,7 @@ const { createReadStream } = require("fs");
 const split2 = require("split2");
 const { Parser, Formatter } = require("pta-tools");
 const temp = require("temp");
+const mclip = require("mclip");
 const { rename } = require("fs/promises");
 const { pipeline } = require("stream/promises");
 const { PassThrough } = require("stream");
@@ -9,30 +10,35 @@ const sort = require("./sorter");
 temp.track();
 
 const defaults = {
-  useStdout: true,
   sort: true,
 };
 
+/**
+ * Cli options
+ * --stdout - Use stdout instead of overwriting the file
+ */
+
 async function main(args, opts) {
   const options = { ...defaults, ...opts };
-  const file = args[0];
-  const readStream = createReadStream(file);
-  const write = temp.createWriteStream();
+  const argsOptions = mclip(args);
+  for (const file of argsOptions.list) {
+    const readStream = createReadStream(file);
+    const write = temp.createWriteStream();
+    const parser = new Parser();
+    const formatter = new Formatter();
 
-  const parser = new Parser();
-  const formatter = new Formatter();
+    await pipeline(
+      readStream,
+      split2(),
+      parser,
+      options.sort ? sort() : new PassThrough({ objectMode: true }),
+      formatter,
+      argsOptions.stdout ? process.stdout : write
+    );
 
-  await pipeline(
-    readStream,
-    split2(),
-    parser,
-    options.sort ? sort() : new PassThrough({ objectMode: true }),
-    formatter,
-    options.useStdout ? process.stdout : write
-  );
-
-  if (!options.useStdout) {
-    await rename(write.path, file);
+    if (!argsOptions.stdout) {
+      await rename(write.path, file);
+    }
   }
 }
 module.exports = main;
